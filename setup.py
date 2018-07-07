7c90eeb6ca0dad50e4c858c98913663189a3b14e
Tim Seed <tim@sy-edm.com>
2018-07-07 07:59:53 +0400

    New Setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='Ham',
-    version='1.0.0.13',
-    packages=['Ham', 'Ham.rbn', 'Ham.adif', 'Ham.calc', 'Ham.dxcc', 'Ham.mode', 'Ham.mode.morse',
-              'Ham.radio', 'Ham.beacon', 'Ham.qsosvr', 'Ham.telnet', 'Ham.antenna', 'Ham.antenna.quad',
-              'Ham.antenna.wind_force', 'Ham.HamTest', 'Ham.rotator', 'Ham.rbn'],
-    url='',
-    license='',
+    version='1.0.0.14',
+    packages=['Ham', 'Ham.rbn', 'Ham.adif', 'Ham.calc', 'Ham.dxcc', 'Ham.mode', 'Ham.mode.morse', 'Ham.radio',
+              'Ham.beacon', 'Ham.qsosvr', 'Ham.telnet', 'Ham.antenna', 'Ham.antenna.quad', 'Ham.antenna.wind_force',
+              'Ham.HamTest', 'Ham.rotator',],
+    url='https://github.com/timseed/Ham.git',
+    license='GNU',
     author='tim seed',
     author_email='tim@sy-edm.com',
     description='My Ham Radio Library. import Ham .... and you are ready to go'
